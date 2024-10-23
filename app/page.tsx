7028f6bd9ce187284639c74a0a92536d9abceb99
Yi Zhang <yi.zhang.yzfl@gmail.com>
2024-10-23 00:45:58 -0400

    Create search bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
+"use client"
+import GeoSearch from "@/app/ui/geosearch";
+import { useEffect } from "react";
 
 export default function Page() {
+  // useEffect(() => {
+  //   navigator.geolocation.getCurrentPosition((position) => {
+  //     console.log(position.coords.latitude)
+  //     console.log(position.coords.longitude)
+  //   })
+  // })
   return (
     <div>
       <div className="grid grid-rows-3 items-center justify-items-center min-h-screen">
         <p className="">
           Hello
         </p>
-        <p className="">
-          Hello This might be a prompt
-        </p>
+        <GeoSearch/>
       </div>
     </div>
   );
---
+++ app/ui/geosearch.tsx
@@ -0,0 +1,7 @@
+export default function GeoSearch() {
+    return (
+        <div className="w-[50vw] text-xl border-2 p-2 flex flex-col focus-within:border-2 focus-within:ring-4">
+            <input className="inset-5 bg-background focus:outline-none"/>
+        </div>
+    )
+}
