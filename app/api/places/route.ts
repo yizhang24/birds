b00d7625ae8828b5e5f4edbbac1392ffca2a766b
Yi Zhang <yi.zhang.yzfl@gmail.com>
2024-10-24 12:38:03 -0400

    Rate limit requests

--- app/api/places/route.ts
+++ app/api/places/route.ts
@@ -1,6 +1,33 @@
 import { NextRequest } from "next/server";
 
+const idToRequests = new Map<string, number>();
+const limitParams = {
+    windowStart: Date.now(),
+    windowSize: 5000, // Five second
+    maxRequests: 10 // Ten requests per five seconds
+}
+
+const limit = (ip: string) => {
+    const now = Date.now();
+    const reset = now - limitParams.windowStart > limitParams.windowSize;
+    if (reset) {
+        limitParams.windowStart = now;
+        idToRequests.set(ip, 0);
+    }
+    const tally = idToRequests.get(ip) ?? 0;
+    if (tally >= limitParams.maxRequests) return true;
+    idToRequests.set(ip, tally + 1);
+    return false;
+}
+
+
 export async function GET(req: NextRequest) {
+    const ip = req.ip ?? req.headers.get('X-Forwarded-For') ?? "";
+    if(limit(ip)) {
+        return new Response(JSON.stringify({"message": "rate limited"}), {
+            status: 429
+        });
+    }
     const searchParams = req.nextUrl.searchParams
     const query = searchParams.get('query')
     const url = `https://maps.googleapis.com/maps/api/place/autocomplete/` +
@@ -15,8 +42,8 @@ export async function GET(req: NextRequest) {
             status: 200
         })
     } catch (err) {
-        return new Response(JSON.stringify({"message" : "Failed to fetch data"}), {
-            status:500
+        return new Response(JSON.stringify({ "message": "Failed to fetch data" }), {
+            status: 500
         })
     }
 }
